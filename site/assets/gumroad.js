c5dbc04bdefc1263565957f8e2b69e839e157f00
root <root@lp.localdomain>
2026-06-29 03:39:57 -0700

    Card autopilot: platform checkout links

--- site/assets/gumroad.js
+++ site/assets/gumroad.js
@@ -1,4 +1,20 @@
 window.GUMROAD = {
   "username": null,
-  "products": {}
+  "products": {
+    "etsy-tag-finder-pro": {
+      "title": "etsy-tag-finder-pro",
+      "price": 14,
+      "url": "https://itch.io/login"
+    },
+    "listinglab-pro": {
+      "title": "listinglab-pro",
+      "price": 19,
+      "url": "https://itch.io/login"
+    },
+    "seller-kit-bundle": {
+      "title": "seller-kit-bundle",
+      "price": 29,
+      "url": "https://itch.io/login"
+    }
+  }
 };
